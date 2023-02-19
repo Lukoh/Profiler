57d5701adb9951746de4ca5bd6bc99afb678cb03
namlukoh <lukoh.nam@bigpearl.io>
2023-02-19 22:01:18 +0900

    Updated UI with new Composable function

--- app/src/main/java/com/goforer/composetest/presentation/ui/profile/SeachSection.kt
+++ app/src/main/java/com/goforer/composetest/presentation/ui/profile/SeachSection.kt
@@ -1,13 +1,15 @@
 package com.goforer.composetest.presentation.ui.profile
 
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
@@ -22,38 +24,53 @@ import com.goforer.composetest.R
 fun SearchSection(modifier: Modifier = Modifier, onTextChanged: (String) -> Unit) {
     var text by remember { mutableStateOf("") }
 
-    TextField(
-        value = text,
-        onValueChange = {
-            text = it
-            onTextChanged(text)
-        },
-        leadingIcon = {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .background(colorResource(id = R.color.colorSystemBgSecondary))
+            .wrapContentHeight(Alignment.Top)
+            .fillMaxWidth()
+    ) {
+        TextField(
+            value = text,
+            onValueChange = {
+                text = it
+                onTextChanged(text)
+            },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = null
+                )
+            },
+            colors = TextFieldDefaults.textFieldColors(
+                placeholderColor = MaterialTheme.colorScheme.surface,
+                textColor = Color.Gray,
+                disabledTextColor = Color.Transparent,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent
+            ),
+            shape = MaterialTheme.shapes.small,
+            placeholder = {
+                Text(stringResource(R.string.placeholder_search))
+            },
+            textStyle = TextStyle.Default.copy(
+                fontSize = 15.sp,
+                fontFamily = FontFamily.SansSerif,
+                fontStyle = FontStyle.Normal,
+                fontWeight = FontWeight.Normal
+            ),
+            modifier = modifier
+                .weight(4f)
+        )
+        SearchIconButton(onClick = { onTextChanged(text) }, icon = {
             Icon(
                 imageVector = Icons.Default.Search,
-                contentDescription = null
+                contentDescription = null,
             )
-        },
-        colors = TextFieldDefaults.textFieldColors(
-            placeholderColor = MaterialTheme.colorScheme.surface,
-            textColor = Color.Gray,
-            disabledTextColor = Color.Transparent,
-            focusedIndicatorColor = Color.Transparent,
-            unfocusedIndicatorColor = Color.Transparent,
-            disabledIndicatorColor = Color.Transparent
-        ),
-        shape = MaterialTheme.shapes.small,
-        placeholder = {
-            Text(stringResource(R.string.placeholder_search))
-        },
-        textStyle = TextStyle.Default.copy(
-            fontSize = 15.sp,
-            fontFamily = FontFamily.SansSerif,
-            fontStyle = FontStyle.Normal,
-            fontWeight = FontWeight.Normal
-        ),
-        modifier = modifier
-            .fillMaxWidth()
-            .heightIn(min = 56.dp)
-    )
+        }, text = {
+            Text("Search")
+        })
+    }
 }
---
+++ app/src/main/java/com/goforer/composetest/presentation/ui/profile/SearchIconButton.kt
@@ -0,0 +1,56 @@
+package com.goforer.composetest.presentation.ui.profile
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SearchIconButton(
+    onClick: () -> Unit,
+    icon: @Composable () -> Unit,
+    text: @Composable () -> Unit,
+    modifier: Modifier = Modifier,
+    interactionSource: MutableInteractionSource =
+        remember { MutableInteractionSource() },
+) {
+    val isPressed by interactionSource.collectIsPressedAsState()
+    val isButtonEnabled = remember { mutableStateOf(true) }
+    /*
+    val animatedButtonColor = animateColorAsState(
+        targetValue = if (isButtonEnabled.value) Color.White else Color.Gray,
+        animationSpec = tween(1000, 0, LinearEasing))
+
+     */
+
+    Button(
+        onClick = onClick,
+        modifier = modifier.wrapContentWidth().heightIn(54.dp),
+        shape = MaterialTheme.shapes.small,
+        interactionSource = interactionSource
+    ) {
+        AnimatedVisibility(visible = isPressed) {
+            if (isPressed) {
+                Row {
+                    icon()
+                    Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+                }
+            }
+        }
+        text()
+    }
+}
